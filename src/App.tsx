29f848341b5cb35ec1a8cdc5008c49fd4591e023
veryMidDev <htvatwork@gmail.com>
2025-05-01 21:52:43 +0700

    feat: play and reverse logo animation on scroll enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from "react";
+import { animate, onScroll } from "animejs";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    animate(".logo", {
+      x: "15rem",
+      rotate: "1turn",
+      autoplay: onScroll({
+        container: ".scroll-container",
+        sync: "play reverse",
+        debug: true,
+      }),
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-red-400">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-screen h-fit container flex flex-col">
+      <div className="h-[120vh] w-full" />
+      <img src={reactLogo} className="logo size-20" alt="React logo" />
+      <div className="h-[30vh] w-full" />
+    </div>
   );
 }
 
